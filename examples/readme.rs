b0fb4c4a81f59cda92a8388a7f0f0ca44a837a98
vimwitch <jchancehud@gmail.com>
2024-09-09 17:10:27 -0700

    chore: lint

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,12 +1,9 @@
+use anyhow::Result;
 use poseidon_bn128::poseidon;
 use scalarff::Bn128FieldElement;
-use anyhow::Result;
 
 fn main() -> Result<()> {
-    let inputs = vec![
-        Bn128FieldElement::from(99),
-        Bn128FieldElement::from(100)
-    ];
+    let inputs = vec![Bn128FieldElement::from(99), Bn128FieldElement::from(100)];
     let result = poseidon(2, &inputs)?;
     println!("{:?}", result);
     Ok(())
